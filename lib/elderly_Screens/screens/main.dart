452623e993e8ac746d891e278464594be2f4af70
Danyh9 <444201030@student.ksu.edu.sa>
2025-10-14 03:56:05 +0300

    Update main.dart

--- lib/elderly_Screens/screens/main.dart
+++ lib/elderly_Screens/screens/main.dart
@@ -1,18 +1,1 @@
-import 'package:flutter/material.dart';
-import '../screens/elderly_home.dart';
 
-void main() {
-  runApp(const KhalilApp());
-}
-
-class KhalilApp extends StatelessWidget {
-  const KhalilApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(), //go to the elderly homepage
-    );
-  }
-}
