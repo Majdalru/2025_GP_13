a78cf9ca8f2f8e107deaf0cf3d06f995bc2ccc4f
Majdalru <444200722@student.ksu.edu.sa>
2025-10-16 03:23:40 +0300

    zz

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
-    id("com.android.application") version "8.7.3" apply false
-    id("com.android.library") version "8.7.3" apply false
+   id("com.android.application") version "8.4.1" apply false 
+    id("com.android.library") version "8.4.1" apply false 
+    
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    // ✅ سطر Google Services (مهم)
     id("com.google.gms.google-services") version "4.4.2" apply false
+    // ✅ سطر Google Services (مهم)
 }
 
 allprojects {
